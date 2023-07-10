5e8ea291ced576a8969747bb8319b2917da72723
Moksh Mahajan <mokshmahajan008@gmail.com>
2023-07-10 15:41:01 +0530

    minor fixes

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -12,7 +12,7 @@ export const posts = [
     content:
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
     likes: {
-      likeCount: 0,
+      likeCount: 3,
       likedBy: [],
       dislikedBy: [],
     },
@@ -27,7 +27,7 @@ export const posts = [
     content:
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
     likes: {
-      likeCount: 0,
+      likeCount: 2,
       likedBy: [],
       dislikedBy: [],
     },
@@ -42,30 +42,30 @@ export const posts = [
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo elit mi, ut placerat felis finibus non. Nulla facilisi. Integer ut maximus ligula. In hac habitasse platea dictumst. Proin euismod iaculis lacus, nec molestie nisl lobortis a. Nullam tristique vestibulum purus sed viverra. Fusce euismod nibh vitae purus lobortis tempus.",
     likes: {
-      likeCount: 0,
+      likeCount: 4,
       likedBy: [],
       dislikedBy: [],
     },
     username: "johnsmith",
     firstName: "John",
     lastName: "Smith",
-    createdAt: "2023-07-10T10:00:00Z",
-    updatedAt: "2023-07-10T10:00:00Z",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
   },
   {
     _id: "uuid()",
     content:
       "Duis gravida mauris id magna egestas, ut semper dui malesuada. Aenean tincidunt lobortis tincidunt. Aliquam id rutrum dui. Nullam condimentum consectetur ante, sit amet vulputate lectus accumsan in. Phasellus euismod nisi nulla, nec varius turpis venenatis in. Nam dapibus mi id est vestibulum interdum. Maecenas viverra urna turpis, id malesuada lectus blandit sit amet. Donec eu risus vel turpis malesuada tristique. Pellentesque accumsan pharetra lectus, id venenatis lacus aliquam sit amet.",
     likes: {
-      likeCount: 0,
+      likeCount: 1,
       likedBy: [],
       dislikedBy: [],
     },
     username: "janedoe",
     firstName: "Jane",
     lastName: "Doe",
-    createdAt: "2023-07-10T12:00:00Z",
-    updatedAt: "2023-07-10T12:00:00Z",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
   },
   {
     _id: uuid(),
@@ -79,7 +79,7 @@ export const posts = [
     username: "alexjones",
     firstName: "Alex",
     lastName: "Jones",
-    createdAt: "2023-07-10T15:00:00Z",
-    updatedAt: "2023-07-10T15:00:00Z",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
   },
 ];
